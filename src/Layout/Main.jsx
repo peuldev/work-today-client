d04c4963d6d0a592f95e8cf5f95f9b36185a434d
peuldev <arfinpeul523@gmail.com>
2024-01-18 20:50:06 +0600

    update bannar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,12 +4,10 @@ import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div className="max-w-screen-xl mx-auto font-Poppins">
+    <div className="font-Poppins">
       <Navbar></Navbar>
-      <div className="mx-2">
-        <Outlet></Outlet>
-        <Footer></Footer>
-      </div>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,17 +1,31 @@
 import team1 from "../../../assets/slider/team1.jpg";
 const Banner = () => {
   return (
-    <div className="h-[800px] rounded" style={{ backgroundImage: `url(${team1})` }}>
+    <div
+      className="h-[800px]"
+      style={{
+        backgroundImage: `url(${team1})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <div className="flex justify-center pt-[300px]">
-        <div className="text-center text-white">
+        <div className="text-center text-white cursor-default">
           <h1 className="text-xl bg-red rounded py-1 inline px-5">
             Welcome To Work Today
           </h1>
-          <h4 className="font-Jost text-6xl font-bold my-5">
+          <h4 className="font-Jost lg:text-6xl md:text-3xl text-xl font-bold my-5">
             Empowering leaders <br /> to live life on{" "}
             <span className="text-red">Purpose</span>
           </h4>
-          <p>Find out more</p>
+          <div className="flex justify-center ">
+            <div className="flex items-center hover:text-red ">
+              <p className="bg-black text-black inline py-3 px-5 rounded-full relative right-4 ">
+                .
+              </p>
+              <p className="cursor-pointer  absolute text-xl">Find out more</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
