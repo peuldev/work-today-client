e2dda37453f99b51e7d76cded057420199e4a9b0
peuldev <arfinpeul523@gmail.com>
2024-01-18 16:39:45 +0600

    init project set pages

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Main;
