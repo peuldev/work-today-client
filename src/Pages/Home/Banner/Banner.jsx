a58a5edef70162a7cbcb5e51042e64115ab01fa3
peuldev <arfinpeul523@gmail.com>
2024-01-18 20:26:18 +0600

    add bannar

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,19 @@
+import team1 from "../../../assets/slider/team1.jpg";
 const Banner = () => {
   return (
-    <div>
-      <h1>Banner</h1>
+    <div className="h-[800px] rounded" style={{ backgroundImage: `url(${team1})` }}>
+      <div className="flex justify-center pt-[300px]">
+        <div className="text-center text-white">
+          <h1 className="text-xl bg-red rounded py-1 inline px-5">
+            Welcome To Work Today
+          </h1>
+          <h4 className="font-Jost text-6xl font-bold my-5">
+            Empowering leaders <br /> to live life on{" "}
+            <span className="text-red">Purpose</span>
+          </h4>
+          <p>Find out more</p>
+        </div>
+      </div>
     </div>
   );
 };
