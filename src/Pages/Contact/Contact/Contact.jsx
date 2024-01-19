80a11f5e18c61f174125e368b67b09b8081e307b
peuldev <arfinpeul523@gmail.com>
2024-01-20 01:06:48 +0600

    set contact page

--- src/Pages/Contact/Contact/Contact.jsx
+++ src/Pages/Contact/Contact/Contact.jsx
@@ -1,7 +1,20 @@
+import titleBg from "../../../assets/title/titlebar-bg.png";
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div
+      className="px-2 h-[500px] py-20"
+      style={{
+        backgroundImage: `url(${titleBg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="max-w-screen-xl mx-auto">
+        <div className="text-white font-Jost text-center pt-[200px]">
+          <h1 className="text-5xl">Contact Us</h1>
+          <p className="border-b inline">Work Today</p>
+        </div>
+      </div>
     </div>
   );
 };
