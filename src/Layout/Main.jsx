a01976fbc9b95f1b938ee01bb1414716c92165a2
peuldev <arfinpeul523@gmail.com>
2024-01-20 21:36:33 +0600

    design login page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname.includes("login");
   return (
     <div className="font-Poppins">
-      <Navbar></Navbar>
+      {<Navbar></Navbar>}
       <Outlet></Outlet>
-      <Footer></Footer>
+      {<Footer></Footer>}
     </div>
   );
 };
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,63 @@
+import { Link } from "react-router-dom";
+import loginBg from "../../assets/images/login.jpg";
+
 const Login = () => {
   return (
-    <div className="px-2 bg-red">
+    <div className="px-2">
       <div className="max-w-screen-xl mx-auto py-20">
-        <div>
-          <h1>login</h1>
+        <div className="hero min-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <div className="card lg:w-1/2">
+              <form className="card-body">
+                <h1 className="text-3xl font-bold">Login</h1>
+                <p>Login to your account.</p>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">E-mail Address</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Type email"
+                    className="input input-bordered"
+                    required
+                    name="email"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    className="input input-bordered"
+                    required
+                    name="password"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="btn text-white bg-red hover:bg-[#D24545]"
+                    type="submit"
+                    value="Login"
+                  />
+                </div>
+                <div>
+                  <p>
+                    <small>
+                      Don’t have an account yet ?{" "}
+                      <Link className="underline text-red" to="/signup">
+                        Join Now
+                      </Link>{" "}
+                    </small>
+                  </p>
+                </div>
+              </form>
+            </div>
+            <div className="text-center lg:w-1/2 lg:text-left">
+              <img className="rounded" src={loginBg} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
