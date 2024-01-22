f0350e9d65b14eb37ec93723c33f9f32800e3173
peuldev <arfinpeul523@gmail.com>
2024-01-22 16:42:32 +0600

    add logo disply show user name

--- src/EmployeeDashboard/EmployeeHome/EmployeeHome.jsx
+++ src/EmployeeDashboard/EmployeeHome/EmployeeHome.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import useAuth from "../../Hooks/useAuth";
 
 const EmployeeHome = () => {
+  const { user } = useAuth();
   return (
     <div>
-      <h1>Employee Home</h1>
+      <h2 className="text-3xl">
+        <span>Hi, Welcome </span>
+        {user?.displayName ? user?.displayName : "back"}
+      </h2>
     </div>
   );
 };
--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,18 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
       {/* dashboard side bar */}
       <div className="w-64 min-h-screen bg-grey">
+        <div>
+          <Link to="/">
+            <p className="font-Jost lg:text-4xl md:text-2xl text-xl font-bold text-center py-4">
+              <span className="text-red">W</span>ork{" "}
+              <span className="text-red">T</span>oday
+            </p>
+          </Link>
+        </div>
         <ul className="menu">
           <li>
             <NavLink to="/dashboard/employeehome">Home</NavLink>
