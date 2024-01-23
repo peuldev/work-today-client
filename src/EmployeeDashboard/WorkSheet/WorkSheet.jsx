6cc8b89e8db5e58aeca2dfc71964dfa88ff9479d
peuldev <arfinpeul523@gmail.com>
2024-01-23 14:49:56 +0600

    add axios secure hooks

--- src/EmployeeDashboard/WorkSheet/WorkSheet.jsx
+++ src/EmployeeDashboard/WorkSheet/WorkSheet.jsx
@@ -3,8 +3,91 @@ import OurTitle from "../../components/OurTitle";
 const WorkSheet = () => {
   return (
     <div>
-      <OurTitle subHeading="Statistics" heading="Today Activity"></OurTitle>
-      <h1>Work Sheet</h1>
+      <div>
+        <OurTitle subHeading="Statistics" heading="Today Activity"></OurTitle>
+      </div>
+      <div>
+        <form className="card-body">
+          <div className="grid lg:grid-cols-2 gap-5">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Location</span>
+            </label>
+            <input
+              type="number"
+              placeholder="location"
+              className="input input-bordered"
+              required
+            />
+          </div>
+
+          <div className="grid lg:grid-cols-3 gap-5">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Hours</span>
+              </label>
+              <input
+                type="number"
+                placeholder="hours"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Selected Task</span>
+              </div>
+              <select className="select select-bordered">
+                <option disabled selected>
+                  Pick one
+                </option>
+                <option>Sales</option>
+                <option>Support</option>
+                <option>Content</option>
+                <option>Paper-work</option>
+                <option>Star Trek</option>
+              </select>
+            </label>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input
+                type="date"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn text-white bg-red">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
