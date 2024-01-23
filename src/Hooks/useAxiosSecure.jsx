f0544efb8e59f9f99ba3c61bb966069655bb4c9f
peuldev <arfinpeul523@gmail.com>
2024-01-23 14:52:14 +0600

    test server url

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://worktoday.vercel.app",
 });
 const useAxiosSecure = () => {
   return axiosSecure;
