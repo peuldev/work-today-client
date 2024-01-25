aa9a5cce987a31a421cb5a7f04258b1aa3224f76
peuldev <arfinpeul523@gmail.com>
2024-01-25 20:05:53 +0600

    disply show all user

--- src/Dashboard/AdminDashboard/AllEmployeeList/AllEmployeeList.jsx
+++ src/Dashboard/AdminDashboard/AllEmployeeList/AllEmployeeList.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import useAllUser from "../../../Hooks/useAllUser";
+import ShowList from "./ShowList";
 
 const AllEmployeeList = () => {
+  const [registerUsers] = useAllUser();
   return (
     <div>
-      <h1>All Employee List</h1>
+      <h1 className="text-4xl font-semibold font-Jost py-5 text-center">
+        All Employee List {registerUsers.length}
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead className="bg-red text-white">
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Designation</th>
+              <th>HR</th>
+              <th>Fire</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {registerUsers.map((registerUser, index) => (
+              <tr key={registerUser._id} className="uppercase">
+                <th>{index + 1}</th>
+                <td>{registerUser.name}</td>
+                <td>{registerUser.designation}</td>
+                <td>Make HR</td>
+                <td>Fire</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/Hooks/useAllUser.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+const useAllUser = () => {
+  const [registerUser, setRegisterUser] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => {
+        setRegisterUser(data);
+      });
+  }, []);
+  return [registerUser];
+};
+
+export default useAllUser;
