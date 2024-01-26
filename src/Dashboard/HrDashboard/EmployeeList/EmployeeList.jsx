5626222c30c8fa3c02f45c4ce9b7d87b983c761d
peuldev <arfinpeul523@gmail.com>
2024-01-26 15:54:21 +0600

    employees verified

--- src/Dashboard/HrDashboard/EmployeeList/EmployeeList.jsx
+++ src/Dashboard/HrDashboard/EmployeeList/EmployeeList.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import useAllUser from "../../../Hooks/useAllUser";
+import Swal from "sweetalert2";
 
 const EmployeeList = () => {
-  const [allemployees] = useAllUser();
+  const [allemployees, setRegisterUser] = useAllUser();
   const employees = allemployees.filter(
     (item) => item.designation === "employee"
   );
+  const handleVerified = (id) => {
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            title: "Employees Verified Successfully",
+            text: "You won't be able to revert this!",
+            icon: "success",
+            showCancelButton: false,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ok",
+          });
+          const remaining = employees.filter((item) => item._id !== id);
+          const update = employees.find((item) => item._id === id);
+          update.status = "confirm";
+          const newRegister = [update, ...remaining];
+          setRegisterUser(newRegister);
+        }
+      });
+  };
   return (
     <div>
       <h1 className="text-4xl font-semibold font-Jost py-5 text-center">
@@ -33,7 +63,18 @@ const EmployeeList = () => {
                   <th>{index + 1}</th>
                   <td>{employee.name}</td>
                   <td>{employee.email}</td>
-                  <td>Yes</td>
+                  <td>
+                    {employee.status === "confirm" ? (
+                      <span>✅</span>
+                    ) : (
+                      <button
+                        className="uppercase"
+                        onClick={() => handleVerified(employee._id)}
+                      >
+                        ❌
+                      </button>
+                    )}
+                  </td>
                   <td>Ac {employee.account}</td>
                   <td>$ {employee.salary}</td>
                   <td className="hover:text-red cursor-pointer">
