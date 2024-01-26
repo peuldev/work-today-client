dc650908666cd874972f44df676c4f3bf8ee3ef3
peuldev <arfinpeul523@gmail.com>
2024-01-26 17:34:17 +0600

    show work seet

--- src/Dashboard/HrDashboard/EmployeeProgress/EmployeeProgress.jsx
+++ src/Dashboard/HrDashboard/EmployeeProgress/EmployeeProgress.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const EmployeeProgress = () => {
+  const [employeeProgress, setEmployeeProgress] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/work-sheet")
+      .then((res) => res.json())
+      .then((data) => setEmployeeProgress(data));
+  }, []);
+  console.log(employeeProgress);
   return (
     <div>
-      <h1>Employee Progress Work Seet</h1>
+      <h1 className="text-4xl font-semibold font-Jost py-5 text-center">
+        Employee Progress Work Seet
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead className="bg-red text-white">
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Hours</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {employeeProgress.map((employeeProgres, index) => (
+              <tr key={employeeProgres._id}>
+                <th>{index + 1}</th>
+                <td>{employeeProgres.name}</td>
+                <td>{employeeProgres.email}</td>
+                <td>{employeeProgres.hours} h</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
